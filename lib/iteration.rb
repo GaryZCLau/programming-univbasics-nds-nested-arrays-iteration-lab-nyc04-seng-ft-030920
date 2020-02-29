12b9cfd5dbd90ada0415ea47114e4869e9a89091
benign-fork-5056 <gareeeliu@gmail.com>
2020-02-29 05:10:02 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -47,12 +47,10 @@ def find_greater_pair(src)
   row_index = 0
     while row_index < src.count do
       element_index = 0
-        while element_index < src[row_index].count do
           if src[row_index][element_index] > src[row_index][element_index + 1]
             greaterarray.push(src[row_index][element_index])
           else src[row_index][element_index] < src[row_index][element_index + 1]
             greaterarray.push(src[row_index][element_index + 1])
-
         end
         row_index += 1
     end
